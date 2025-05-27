4ede2ee3ddeaaa8b5679c4e307c1d8fdc7e7c9dc
ZenChris <121231422+ZenChris18@users.noreply.github.com>
2025-05-28 01:44:04 +0800

    added files

---
+++ README.md
@@ -0,0 +1,14 @@
+# simulate
+1. create venv
+2. install dependencies
+3. run main.py
+
+## change this to customize the simulation
+### main.py
+1. number_of_episodes - change this to how many time the agent tries the world
+2. world_size - change this to change the world size "sizexsize"
+### simulation.py
+2. max_steps - change this to let the agent have how many steps in the world (make worldsize x 2 so steps can reach end to end of the world)
+
+
+each episode the agent starts in a random place
---
+++ src/main.py
@@ -0,0 +1,29 @@
+import pygame
+from simulation import run_episode
+from world_generator import generate_world
+from visualize import render
+from agent import Explorer
+
+# Initialize
+world_size = 100 # Size of the world and agent
+world = generate_world(size=world_size)
+agent = Explorer(grid_size=world_size)
+pygame.init()
+screen = pygame.display.set_mode((400, 400))
+number_of_episodes = 100 # number of tries the agent will do to learn
+
+# Training loop
+for episode in range(number_of_episodes):
+    # world = generate_world(size=world_size)  # Regenerate world for each episode remove if want to keep the same world
+    total_reward, final_x, final_y = run_episode(world, agent, episode=episode)
+    agent.epsilon = max(agent.epsilon_min, agent.epsilon * agent.epsilon_decay)
+    if total_reward < 100 and episode > 30:
+        agent.epsilon = min(0.5, agent.epsilon + 0.1)
+
+    print(f"Episode {episode}: Reward = {total_reward}")
+
+    # Render once per episode so i can see
+    render(screen, world, (final_x, final_y)) 
+    pygame.display.update()
+
+pygame.quit()
